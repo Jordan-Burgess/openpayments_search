0bdbfe89bcb0ef47f4ee85b1b813e3d604cd612a
Jordan-Burgess <alfredjordan93@gmail.com>
2024-02-27 10:50:23 -0500

    Create a Payment model with all relevant data fields

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Payment(models.Model):
+    doctor_profile_id = models.IntegerField()
+    doctor_npi = models.IntegerField()
+    doctor_first_name = models.CharField(max_length=50)
+    doctor_middle_name = models.CharField(max_length=50)
+    doctor_last_name = models.CharField(max_length=50)
+    doctor_primary_address_line1 = models.CharField()
+    doctor_primary_address_line2 = models.CharField()
+    doctor_city = models.CharField(max_length=50)
+    doctor_state = models.CharField(max_length=2)
+    doctor_zip_code = models.CharField(max_length=10)
+    doctor_country = models.CharField(max_length=50)
+    doctor_primary_type = models.CharField()
+    doctor_specialty = models.CharField()
+    doctor_license_state_code = models.CharField(max_length=2)
+    submitting_manufacturer_name = models.CharField()
+    submitting_manufacturer_id = models.IntegerField()
+    submitting_manufacturer_state = models.CharField(max_length=2)
+    submitting_manufacturer_country = models.CharField(max_length=50)
+    payment_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    payment_date = models.DateField()
+    payment_quantity = models.IntegerField()
+    payment_type = models.CharField()
+    program_year = models.IntegerField()
+    publication_date = models.DateField()   
+
+
