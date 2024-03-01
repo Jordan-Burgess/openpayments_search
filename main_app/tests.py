63b4789a34172940247bc9befe8a01ba803eb934
Jordan-Burgess <alfredjordan93@gmail.com>
2024-03-01 10:12:07 -0500

    Write test for search match on indexed data

--- main_app/tests.py
+++ main_app/tests.py
@@ -1,5 +1,8 @@
 from django.test import TestCase
 from django.core.management import call_command
+from elasticsearch_dsl.connections import connections
+from elasticsearch.helpers import bulk
+from .documents import PaymentDocument
 from .models import Payment
 from datetime import date
 from unittest import mock
@@ -60,3 +63,42 @@ class ImportOpenPaymentCommandTest(TestCase):
 
         self.assertEqual(Payment.objects.count(), 3)
         self.assertTrue(Payment.objects.filter(doctor_first_name="JUSTIN").exists())
+
+class PaymentIndexTest(TestCase):
+    # Test for search match on indexed data
+    def setUp(self):
+        PaymentDocument.init()
+        self.payment = Payment.objects.create(
+            doctor_profile_id = 1234567,
+            doctor_npi = 1110123456,
+            doctor_first_name = "John",
+            doctor_middle_name = "Test",
+            doctor_last_name = "Smith",
+            doctor_primary_address_line1 = "123 Cherry St.",
+            doctor_primary_address_line2 = "Apt. 123",
+            doctor_city = "New York",
+            doctor_state = "NY",
+            doctor_zip_code = "11413",
+            doctor_country = "United States",
+            doctor_primary_type = "Medical Doctor",
+            doctor_specialty = "Podiatric Medicine",
+            doctor_license_state_code = "NY",
+            submitting_manufacturer_name = "Test Manufacturer",
+            submitting_manufacturer_id = 100000123456,
+            submitting_manufacturer_state = "NY",
+            submitting_manufacturer_country = "United States",
+            payment_amount = 130.00,
+            payment_date = date.today(),
+            payment_quantity = 1,
+            payment_type = "Education",
+            program_year = 2022,
+            publication_date = date.today(),
+        )
+        PaymentDocument().update(self.payment)
+
+    def test_indexing_data(self):
+        search_results = PaymentDocument.search().query('match', doctor_first_name='John').execute()
+        self.assertTrue(search_results.hits.total.value > 0)
+
+    def tearDown(self):
+        self.payment.delete()
