de280dd2d345e295fe1d9e005259cc79d9bed621
Jordan-Burgess <alfredjordan93@gmail.com>
2024-03-01 22:24:03 -0500

    Add CSS to html document

--- main_app/views.py
+++ main_app/views.py
@@ -8,7 +8,7 @@ def search_page(request):
 def api_search(request):
     query = request.GET.get('q', '')
     if query:
-        search = PaymentDocument.search().query('multi_match', query=query, fields=['doctor_first_name', 'doctor_last_name'])
+        search = PaymentDocument.search().query('multi_match', query=query, fields=['doctor_first_name', 'doctor_last_name'], type='phrase_prefix')
         response = search.execute()
         results = [hit.to_dict() for hit in response.hits]
         return JsonResponse(results, safe=False)
