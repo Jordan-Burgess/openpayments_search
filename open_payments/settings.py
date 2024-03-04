aeea80fbdd8ea0ded0036a23fb978006444b69c1
Jordan-Burgess <alfredjordan93@gmail.com>
2024-03-04 09:54:46 -0500

    Add http_auth to elastic_search config

--- open_payments/settings.py
+++ open_payments/settings.py
@@ -90,7 +90,8 @@ elastic_search_url = urlparse(os.getenv('BONSAI_URL'))
 
 ELASTICSEARCH_DSL = {
     'default': {
-        'hosts': [{'host': elastic_search_url.hostname, 'port': 443, 'use_ssl': True}]
+        'hosts': [{'host': elastic_search_url.hostname, 'port': 443, 'use_ssl': True}],
+        'http_auth': (elastic_search_url.username, elastic_search_url.password) if elastic_search_url.username else None
     }
 }
 
