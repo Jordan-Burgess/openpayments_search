1919ec52da7a4c0cb9b8993fcabde5ea27e22e3b
Jordan-Burgess <alfredjordan93@gmail.com>
2024-03-04 22:05:54 -0500

    Add click handler for export to excel button

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import search_page, api_search
+from .views import search_page, api_search, export_to_excel
 
 urlpatterns = [
     path('search/', search_page, name='search_page'),
     path('api/search/', api_search, name='api_search'),
+    path('export_excel/', export_to_excel, name='export_excel'),
 ]
