abc5dab1ea2a0b49ed2619ee48e58de2cfd6d3df
Jordan-Burgess <alfredjordan93@gmail.com>
2024-03-14 10:46:54 -0400

    add payment identifier

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class Payment(models.Model):
+    identifier = models.BigIntegerField()
     doctor_profile_id = models.BigIntegerField()
     doctor_npi = models.BigIntegerField()
     doctor_first_name = models.CharField(max_length=50)
